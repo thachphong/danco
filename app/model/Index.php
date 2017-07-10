b0281dd78279d30e63e9138d0374884d84c6ddc4
thachphong <whirlwind2887@gmail.com>
2017-07-10 23:43:28 +0700

    remove check key

--- app/model/Index.php
+++ app/model/Index.php
@@ -6,11 +6,7 @@ class Index_model extends ACWModel
 	public static function init()
 	{
 		//Login_model::check();	
-		$date= date('Ymd');
-		if($date > '20170718'){
-			echo 'Trang web của bạn đã bị khóa do chưa thanh toán tiền thiết kế web!';
-			die;
-		}
+		
 		
 		//if()
 	}
